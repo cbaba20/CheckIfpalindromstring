67af1cbed8e9d9d42150f8fa8cce1f2f3fc133df
Chandan <cbaba20@gmail.com>
2018-03-24 02:33:29 +0530

    Added palindrome code

--- palindromestring/Program.cs
+++ palindromestring/Program.cs
@@ -8,8 +8,47 @@ namespace palindromestring
 {
     class Program
     {
+        public static bool isPalindrome(string word)
+        {
+            //char[] letters = word.ToCharArray();
+            string[] storeWords = new string[word.Length];
+            string[] storeBackWOrds = new string[word.Length];
+
+            string arrayToWords="", backWOrds="";
+
+            for (int k = 0; k < word.Length; k++)
+            {
+                storeWords[k] = Convert.ToString(word[k]).Trim().ToLower();
+                Console.WriteLine(storeWords[k].ToString());
+                arrayToWords += String.Concat(storeWords[k]);
+
+            }
+            Console.WriteLine("\n");
+            for (int j = word.Length - 1; j > -1; j--)
+            {
+                storeBackWOrds[j] = Convert.ToString(word[j]).Trim().ToLower();
+                Console.WriteLine(storeBackWOrds[j].ToString());
+                backWOrds+= String.Concat(storeWords[j]);
+            }
+
+
+           if(arrayToWords== backWOrds)
+            {
+                Console.WriteLine("{0} and {1} are palindrome", arrayToWords, backWOrds);
+                return true;
+            }
+           else
+            {
+                Console.WriteLine("{0} and {1} are not palindrome", arrayToWords, backWOrds);
+                return false;
+            }
+
+        }
         static void Main(string[] args)
         {
+            string wordToCheck = Convert.ToString(Console.ReadLine());
+            Console.WriteLine(Program.isPalindrome(wordToCheck));
+            Console.ReadLine();
         }
     }
 }
